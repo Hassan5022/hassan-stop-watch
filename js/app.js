c33836839e74849333583344709d7df0a86efb48
Hassan Ahmed <hassanahmed5022@gmail.com>
2020-07-11 18:36:47 +0500

    ready to 2nd commit

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
 var display = document.getElementById('timer');
-var secs = 0;
-var mins = 0;
-var hrs  = 0;
-var h = "";
+var msecs = 0;
+var sec = 0;
+var min  = 0;
 var m = "";
 var s = "";
+var ms = "";
 var timer;
 
 function countTimer(){
-	secs++;
+	msecs++;
 	
-	if(secs >= 60){
-		secs = 0;
-		mins++;
-		if(mins >= 60){
-			mins = 0;
-			hrs++;
+	if(msecs >= 59){
+		msecs = 0;
+		sec++;
+		if(sec >= 59){
+			sec = 0;
+			min++;
 		}
 	}
 	
-	h = hrs ? hrs > 9 ? hrs : "0" + hrs : "00";
-	m = mins ? mins > 9 ? mins : "0" + mins : "00";
-	s = secs > 9 ? secs : "0" + secs;
+	m = min ? min > 9 ? min : "0" + min : "00";
+	s = sec ? sec > 9 ? sec : "0" + sec : "00";
+	ms = msecs > 9 ? msecs : "0" + msecs;
 	
-	display.innerHTML = h+":"+m+":"+s+"s";
+	display.innerHTML = m+":"+s+":"+ms+"s";
 	
 	timerDuration();
 }
 
 function timerDuration(){
-	if(hrs != 99){
-		timer = setTimeout(countTimer, 100);
+	if(m != 99){
+		timer = setTimeout(countTimer, 10);
 	}
 	
 }
@@ -50,10 +50,10 @@ function resetTimer(){
 	document.getElementsByClassName('btn-success')[0].removeAttribute('disabled');
 	clearTimeout(timer);
 	display.innerHTML = "00:00:00s";
-	secs = 0; 
-	mins = 0; 
-	hrs = 0;
-	h = "";
+	msecs = 0; 
+	sec = 0; 
+    min = 0;
 	m = "";
 	s = "";
+	ms = "";
 }
